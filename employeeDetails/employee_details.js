35ff483143889ea1101d93f3d7706580155a6819
bertrand12345 <data@batanganaekobo.ch>
2025-03-31 09:50:14 -0400

    Second commit

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization:'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization:'Java' },
     //... D'autres enregistrements d'employés peuvent être ajoutés ici
   ];
 
@@ -29,6 +29,17 @@ function findEmployeeById(employeeId) {
     }
     else{
       document.getElementById('employeesDetails').innerHTML = 'aucun employé n\'a été trouvé avec cet ID';
+
+    }
+}
+
+function findEmployeeBySpecialization(employeeSpec) {
+    const foundSpec = employees.find(employee => employee.specialization === 'Javascript');
+    if (foundSpec) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundSpec.id}: ${foundSpec.name}  - ${foundSpec.department} - ${foundSpec.specialization}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'aucun employé n\'a été trouvé avec cette specialisation';
 
     }
 }
